403706996f9b5bf22258952fadb6002c03f388a0
mhmdnoman <mhmdnoman01@gmail.com>
2022-06-14 13:00:13 +0600

    finish computerPlay()

---
+++ script.js
@@ -0,0 +1,14 @@
+function computerPlay () {
+    item = ['rock', 'paper', 'scissors']
+    num = Math.floor((Math.random() * 3) + 0);
+    console.log(item[num])
+}
+
+computerPlay();
+
+
+
+// Store items in an array
+// get random number's for 3 items
+// assign rnadom numbers as a index of array
+// print the out put
